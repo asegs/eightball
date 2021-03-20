4fc45af4b0827f3580532bd954e3125e86abaa0f
ajs2290 <ajs2290@g.rit.edu>
2021-03-19 20:32:35 -0400

    Eightball project

---
+++ src/Components/BasicUI.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+class Controls extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            question:'',
+            history:[],
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+}
---
+++ src/Components/HistoryModal.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import Modal from '@material-ui/core/Modal';
+
+class HistoryModal extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            open:false
+        }
+    }
+
+    render() {
+        return(
+
+        )
+    }
+
+}
